a22cc6317798e7fa940cd8419f3595a64ebd4eef
Onoriode Dafetta <onoriodedafetta@gmail.com>
2023-01-22 05:02:38 +0100

    added the socials in the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
+import { socials } from '../constants/index';
 
 const Footer = () => (
   <motion.footer
@@ -40,7 +41,18 @@ const Footer = () => (
 
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">MetaVerse</h4>
-          <p>Copyright © 2022 - 2023 Metaverse. All rights reserved.</p>
+          <p className="font-normal text-white text-[14px]">Copyright © 2022 - 2023 Metaverse. All rights reserved.</p>
+
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
