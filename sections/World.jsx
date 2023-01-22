8d12f07d17a598b9ec8e6914d07f64597f1ee3d7
Onoriode Dafetta <onoriodedafetta@gmail.com>
2023-01-22 05:41:35 +0100

    added animation to the user pins

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,22 +16,22 @@ const World = () => (
     >
       <TypingText title="| People In The MetaVerse!" textStyles="text-center" />
       <TitleText
-        title={(
+        title={
           <>
             Track friends around you and invite them to play together in the
             same MetaVerse
           </>
-        )}
+        }
         textStyles="text-center"
       />
 
       <motion.div
-        variants={fadeIn('up', 'tween', 0.3, 1)}
+        variants={fadeIn("up", "tween", 0.3, 1)}
         className="mt-[68px] flex w-full h-[550px] relative"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
 
-        <div className="bottom-20 absolute right-20 w-[100px] h-[100px] bg-[url('/pin.png')]">
+        <div className="bounce-animation bottom-20 absolute right-20 w-[100px] h-[100px] bg-[url('/pin.png')]">
           <img
             src="/people-01.png"
             alt="people"
@@ -40,7 +40,7 @@ const World = () => (
           <div className="w-full h-full bg-[url('/orange-blur.png')] object-contain rounded-full relative bottom-[5rem]" />
         </div>
 
-        <div className="top-20 rounded-full absolute left-20 w-[100px] h-[100px] bg-[url('/pin.png')] ">
+        <div className="bounce-animation top-20 rounded-full absolute left-20 w-[100px] h-[100px] bg-[url('/pin.png')] ">
           <img
             src="/people-02.png"
             alt="people"
@@ -49,7 +49,7 @@ const World = () => (
           <div className="w-full h-full bg-[url('/pink-blur.png')] object-contain rounded-full relative bottom-[5rem]" />
         </div>
 
-        <div className="top-1/2 rounded-full absolute right-[45%] w-[100px] h-[100px] bg-[url('/pin.png')] ">
+        <div className="bounce-animation top-1/2 rounded-full absolute right-[45%] w-[100px] h-[100px] bg-[url('/pin.png')] ">
           <img
             src="/people-03.png"
             alt="people"
